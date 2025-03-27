201c9a94bb5856d04b8e51a8ac70f390ba1b1913
shariqriazz <ishariqriaz@gmail.com>
2025-03-27 11:45:32 +0500

    Delete .history directory

--- .history/scripts/dev_20250327104355.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const { spawn } = require('child_process');
-
-const port = process.env.PORT || 4269;
-const nextDev = spawn('next', ['dev', '-p', port], { 
-    stdio: 'inherit',
-    shell: true 
-});
--- .history/scripts/port_20250327104256.js
+++
@@ -1,3 +0,0 @@
-// Get port from environment variable or use default
-const port = process.env.PORT || 4269;
-console.log(port);
